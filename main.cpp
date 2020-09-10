58b2812079f90b24b0a7d075dd9fad28b609af30
Danil Tolmachev <tolmachdanil@gmail.com>
2020-09-10 18:03:59 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include "app/App.h"
 #include "routing/Router.h"
 #include <iostream>
+
 int main() {
     auto app = App();
-    auto router =Router();
-    router.addHandler("/", static_cast<MiddlewareFunc>([](Context* context, const NextFunc& next) {
-//            std::cout<<;
-            context->response->body = const_cast<char *>("Ylii Pelevin TOPCHEG!");
-            next();
-        }), {"GET"});
-    router.addHandler("/hello",[](Context* context, const NextFunc& next) {
-        context->response->body = const_cast<char *>("Hello P");
+    auto router = Router();
+    auto f = [](Context *context, const NextFunc &next) {
+        context->response->body = const_cast<char *>("Ylii Pelevin TOPCHEG!");
         next();
-    });
+    };
+    router.addHandler("/", f);
+//    router.addHandler("/hello", [](Context *context, const NextFunc &next) {
+//        context->response->body = const_cast<char *>("Hello P");
+//        next();
+//    });
 
     app.middlewareManager->middleware->push_back(router.getRoutingMiddleware());
     app.start("127.0.0.1:8000");
