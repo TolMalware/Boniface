f837b29480330183d0f73a690aa31ebc8547747d
Golbakh Mikhail <golbah@yandex.ru>
2020-09-20 03:15:17 +0300

    feat(router): add aliases for methods

--- router/Router.h
+++ router/Router.h
@@ -21,6 +21,15 @@ public:
     Router &use(const std::string &path, uint8_t methods, Middleware *middleware);
     Router &use(const std::string &path, uint8_t methods, MiddlewareFunc middleware);
 
+    Router &get(const std::string &path, Middleware *middleware);
+    Router &get(const std::string &path, MiddlewareFunc middleware);
+    Router &post(const std::string &path, Middleware *middleware);
+    Router &post(const std::string &path, MiddlewareFunc middleware);
+    Router &put(const std::string &path, Middleware *middleware);
+    Router &put(const std::string &path, MiddlewareFunc middleware);
+    Router &deleteMethod(const std::string &path, Middleware *middleware);
+    Router &deleteMethod(const std::string &path, MiddlewareFunc middleware);
+
     void cacheLayer(Layer *layer);
 };
 
